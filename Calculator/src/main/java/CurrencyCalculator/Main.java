33b96c95965978f6124784baa3afd4ca782fcbb0
Bartosz Ojczyk <>
2019-06-07 14:08:23 +0200

    commit2
    main

---
+++ Calculator/src/main/java/CurrencyCalculator/Main.java
@@ -0,0 +1,38 @@
+package CurrencyCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args); //uruchomienie springa
+
+    }
+}
+
+
+/*
+Progress :
+
+1. main - Bartek
+2. main controller - Bartek
+
+3. template - home.ftl - Dominika + Bartek
+
+4. getmapping w maincontroller - Niemira + Dominika
+
+5. provider - currency - Bartek
+
+6. currency list - Niemira
+7. converter - Bartek
+
+8. validatorCurrency - Dominika
+
+9. validatorInput - Niemira
+
+10. testCurrency - Dominika
+
+11. testInput - Niemira
+
+ */
